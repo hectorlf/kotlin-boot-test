1f005b691aa9e504e6fd17c9e1f5b8feaaaa5342
hectorlf <hectorlopezfernandez@hotmail.com>
2016-02-27 16:14:36 +0100

    Transfer another class from java to kotlin

--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -17,6 +17,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java)
@@ -77,3 +78,6 @@ open class ApplicationSecurity : WebSecurityConfigurerAdapter() {
     }
 
 }
+
+
+class SessionInitializer: AbstractHttpSessionApplicationInitializer(SessionConfiguration::class.java)
