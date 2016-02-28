20473f9fadbfdfa6a857427dfda2b836b4710514
hectorlf <hectorlopezfernandez@hotmail.com>
2016-02-28 12:38:43 +0100

    Transfer SessionConfiguration from java to kotlin

--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -1,5 +1,7 @@
 package hello
 
+import javax.servlet.ServletContext
+
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -17,11 +19,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.session.ExpiringSession
+import org.springframework.session.MapSessionRepository
+import org.springframework.session.SessionRepository
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer
+import org.springframework.session.web.http.CookieHttpSessionStrategy
+import org.springframework.session.web.http.HttpSessionStrategy
+import org.springframework.session.web.http.MultiHttpSessionStrategy
+import org.springframework.session.web.http.SessionRepositoryFilter
+import org.springframework.beans.factory.annotation.Autowired
+
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java)
 }
+
 
 @SpringBootApplication(exclude = arrayOf(SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class))
 @ServletComponentScan
@@ -45,7 +57,9 @@ open class Application {
 @EnableConfigurationProperties
 open class ApplicationSecurity : WebSecurityConfigurerAdapter() {
 
-    private val MANAGEMENT_ENDPOINTS = arrayOf("/management/dump", "/management/health", "/management/metrics", "/management/trace")
+	private object Endpoints {
+		val MANAGEMENT_ENDPOINTS = arrayOf("/management/dump", "/management/health", "/management/metrics", "/management/trace")
+	}
 
     @Bean
     @ConditionalOnMissingBean
@@ -64,8 +78,8 @@ open class ApplicationSecurity : WebSecurityConfigurerAdapter() {
         if (props.headers.hsts != SecurityProperties.Headers.HSTS.NONE) http.headers().httpStrictTransportSecurity().includeSubDomains(props.headers.hsts == SecurityProperties.Headers.HSTS.ALL)
         http.sessionManagement().sessionCreationPolicy(props.sessions)
         // management access rules
-        http.requiresChannel().antMatchers(*MANAGEMENT_ENDPOINTS).requiresSecure()
-        http.authorizeRequests().antMatchers(*MANAGEMENT_ENDPOINTS).hasRole("ADMIN").and().httpBasic()
+        http.requiresChannel().antMatchers(*Endpoints.MANAGEMENT_ENDPOINTS).requiresSecure()
+        http.authorizeRequests().antMatchers(*Endpoints.MANAGEMENT_ENDPOINTS).hasRole("ADMIN").and().httpBasic()
         // app access rules
         http.authorizeRequests().antMatchers("/secure/**").hasRole("USER").and().httpBasic()
         // default access rules
@@ -81,3 +95,31 @@ open class ApplicationSecurity : WebSecurityConfigurerAdapter() {
 
 
 class SessionInitializer: AbstractHttpSessionApplicationInitializer(SessionConfiguration::class.java)
+
+@Configuration
+open class SessionConfiguration {
+
+	private val defaultHttpSessionStrategy = CookieHttpSessionStrategy()
+	
+	var httpSessionStrategy: HttpSessionStrategy = defaultHttpSessionStrategy
+		@Autowired(required = false) set
+	
+	@Autowired(required = false)
+	var servletContext: ServletContext? = null
+	
+	@Bean
+	open fun sessionRepository(): SessionRepository<ExpiringSession> = MapSessionRepository()
+
+	@Bean
+	open fun <S: ExpiringSession> springSessionRepositoryFilter(sessionRepository: SessionRepository<S>): SessionRepositoryFilter<out ExpiringSession> {
+		val sessionRepositoryFilter = SessionRepositoryFilter<S>(sessionRepository)
+		sessionRepositoryFilter.setServletContext(servletContext)
+		if (httpSessionStrategy is MultiHttpSessionStrategy) {
+			sessionRepositoryFilter.setHttpSessionStrategy(httpSessionStrategy as MultiHttpSessionStrategy)
+		} else {
+			sessionRepositoryFilter.setHttpSessionStrategy(httpSessionStrategy)
+		}
+		return sessionRepositoryFilter
+	}
+
+}
